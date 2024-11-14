d933aa2ea1e03a5fa55472259cf55644724120fd
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-14 16:52:26 +0100

    fix console.log and modify modul order

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 for (let i = 1; i<=100; i++){
   let parola = '';
-  if (i % 3){
-    parola = 'Fizz';
+  if (!(i % 3) && !(i % 5)){
+    parola = 'FizzBuzz';
   }
-  else if (i % 5){
+  else if (!(i % 5)){
     parola = 'Buzz';
   }
-  else if (i % 3 && i % 5){
-    parola = 'BuzzFizz';
+  else if (!(i % 3)){
+    parola = 'Fizz';
   }else{
     parola = i;
   }
