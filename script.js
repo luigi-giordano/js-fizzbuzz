94dfb22eda0b03bc5632062b6831a91b9d4d0a3b
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-14 17:28:06 +0100

    add comments in js and end of excercise

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+// SOLUZIONE 1
+// PAROLACHIAVE CICLO
 for (let i = 1; i<=100; i++){
+  // VARIABILE
   let parola = '';
+  // ISTRUZIONE CONDIZIONALE
   if (!(i % 3) && !(i % 5)){
     parola = 'FizzBuzz';
   }
+  // ISTRUZIONE CONDIZIONALE
   else if (!(i % 5)){
     parola = 'Buzz';
   }
+  // ISTRUZIONE CONDIZIONALE
   else if (!(i % 3) ){
     parola = 'Fizz';
+    // ISTRUZIONE CONDIZIONALE ALTERNATIVA
   }else{
     parola = i;
   }
